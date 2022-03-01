6b6d3094be9663a4e8eda2c7cf3c821d92f989b3
ductruong253 <ddtruong94@gmail.com>
2022-03-01 21:13:54 +0700

    Add Lab01 ex17

---
+++ Lab01/Exercise17.js
@@ -0,0 +1,17 @@
+/*
+Bài tập 17.
+Tính S(n) = n!
+Trong đó n được nhập từ người dùng.
+*/
+
+const n = 3;
+
+function sumT(n) {
+    let result = 1;
+    for (i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+console.log('T(x, n) = ', sumT(n));
